7ab7777a05dbf3b80c809bd64602dd1c8ab57174
haebeen <haebing0309@gmail.com>
2023-11-14 15:24:11 +0900

    Initial commit

---
+++ src/main/java/com/project/dogwalker/DogwalkerApplication.java
@@ -0,0 +1,13 @@
+package com.project.dogwalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogwalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogwalkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dogwalker/DogwalkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dogwalker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogwalkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
