0f8cd23282416c083a89f006cebe894e4b4badb7
haebeen <haebing0309@gmail.com>
2023-11-22 16:21:10 +0900

    [fix] 구글 client json으로 데이터 전달해야하는 에러 해결

--- src/main/java/com/project/dogwalker/member/client/goolge/GoogleClient.java
+++ src/main/java/com/project/dogwalker/member/client/goolge/GoogleClient.java
@@ -1,18 +1,17 @@
 package com.project.dogwalker.member.client.goolge;
 
-import com.project.dogwalker.member.dto.google.GoogleInfRequest;
 import com.project.dogwalker.member.dto.ClientResponse;
+import com.project.dogwalker.member.dto.google.GoogleInfRequest;
 import com.project.dogwalker.member.dto.google.GoogleRequest;
 import com.project.dogwalker.member.dto.google.GoogleResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "google",url = "${google.auth.url}")
 public interface GoogleClient {
 
   @PostMapping("/token")
-  GoogleResponse getGoogleToken(@RequestParam GoogleRequest googleRequest);
+  GoogleResponse getGoogleToken(GoogleRequest googleRequest);
 
   @PostMapping("/tokeninfo")
   ClientResponse getGoogleDetailInfo(GoogleInfRequest googleInfRequest);
