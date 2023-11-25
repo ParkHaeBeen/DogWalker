13dfeec7392f03d60f83cf2e12bb7a85a3dfc81c
haebeen <haebing0309@gmail.com>
2023-11-25 14:24:23 +0900

    [refactoring] error 내용 추가, exception 수정

--- src/main/java/com/project/dogwalker/exception/ErrorCode.java
+++ src/main/java/com/project/dogwalker/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
   FEIGN_BAD_REQUEST("잘못된 요청입니다"),
   FEIGN_PARSE_ERROR("파싱 에러가 발생했습니다"),
   FEIGN_ERROR("feign 예외 발생했습니다"),
-  NOT_WRITE_SERVICE_PRICE("서비수 수행자는 회원가입시 서비스 수행에 대한 가격을 기입해야합니다");
+  NOT_WRITE_SERVICE_PRICE("서비수 수행자는 회원가입시 서비스 수행에 대한 가격을 기입해야합니다"),
+  NOT_EXIST_MEMBER("멤버가 존재하지 않습니다. 회원가입해주세요");
   private final String value;
 
   ErrorCode(final String value) {
--- src/main/java/com/project/dogwalker/exception/member/WalkerNotWritePriceException.java
+++ src/main/java/com/project/dogwalker/exception/member/WalkerNotWritePriceException.java
@@ -8,10 +8,6 @@ import lombok.Getter;
 @Getter
 public class WalkerNotWritePriceException extends CustomException {
 
-  public WalkerNotWritePriceException(final ErrorCode errorCode ,final String message) {
-    super(errorCode , message);
-  }
-
   public WalkerNotWritePriceException(final ErrorCode errorCode){
     super(errorCode);
   }
