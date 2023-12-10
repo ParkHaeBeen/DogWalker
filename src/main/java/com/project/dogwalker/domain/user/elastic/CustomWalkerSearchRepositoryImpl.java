98af9222686b57986882aefcc02eb8008da0d182
haebeen <haebing0309@gmail.com>
2023-12-10 14:58:30 +0900

    [test] elastcisearch 에러 해결및 테스트

--- src/main/java/com/project/dogwalker/domain/user/elastic/CustomWalkerSearchRepositoryImpl.java
+++ src/main/java/com/project/dogwalker/domain/user/elastic/CustomWalkerSearchRepositoryImpl.java
@@ -5,18 +5,17 @@ import com.project.dogwalker.walkerSearch.dto.WalkerInfoSearchCond;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.client.erhlc.NativeSearchQuery;
-import org.springframework.data.elasticsearch.client.erhlc.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
-
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 
 
 @RequiredArgsConstructor
@@ -24,8 +23,8 @@ public class CustomWalkerSearchRepositoryImpl implements CustomWalkerSearchRepos
 
   private final ElasticsearchOperations elasticsearchOperations;
 
-
-  private final double distanceMax=10;
+  @Value("${spring.elastic.distance}")
+  private final int distanceMax;
 
   @Override
   public Page <WalkerDocument> searchByName(final WalkerInfoSearchCond walkerInfoSearchCond) {
@@ -33,7 +32,7 @@ public class CustomWalkerSearchRepositoryImpl implements CustomWalkerSearchRepos
         walkerInfoSearchCond.getSize());
 
 
-    NativeSearchQuery query1=createCond(walkerInfoSearchCond,pageable);
+    CriteriaQuery query1=createCond(walkerInfoSearchCond,pageable);
     SearchHits <WalkerDocument> searchHits = elasticsearchOperations.search(query1 , WalkerDocument.class);
 
     List <WalkerDocument> list = searchHits.stream().map(SearchHit::getContent)
@@ -43,16 +42,13 @@ public class CustomWalkerSearchRepositoryImpl implements CustomWalkerSearchRepos
   }
 
 
-  private NativeSearchQuery createCond(final WalkerInfoSearchCond walkerInfoSearchCond ,final Pageable pageable){
+  private CriteriaQuery createCond(final WalkerInfoSearchCond walkerInfoSearchCond ,final Pageable pageable){
+    Criteria nameCriteria = Criteria.where("walker_name").contains(walkerInfoSearchCond.getWalkerName())
+        .and("location").within(new GeoPoint(walkerInfoSearchCond.getLat(),walkerInfoSearchCond.getLnt()),distanceMax+"km");
 
-    GeoPoint bottomRight = new GeoPoint(12.0, 4.0);
-    GeoPoint topLeft = new GeoPoint(14.5, 1.0);
+    CriteriaQuery criteriaQuery = new CriteriaQuery(nameCriteria);
+    criteriaQuery.setPageable(pageable);
 
-    NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
-        .withQuery(QueryBuilders.geoBoundingBoxQuery("location")
-            .setCorners(topLeft.getLat(), topLeft.getLon(), bottomRight.getLat(), bottomRight.getLon()))
-        .build();
-
-    return searchQuery;
+    return criteriaQuery;
   }
 }
--- src/test/java/com/project/dogwalker/domain/user/elastic/CustomWalkerSearchRepositoryImplTest.java
+++ src/test/java/com/project/dogwalker/domain/user/elastic/CustomWalkerSearchRepositoryImplTest.java
@@ -3,6 +3,8 @@ package com.project.dogwalker.domain.user.elastic;
 import com.project.dogwalker.domain.user.Role;
 import com.project.dogwalker.domain.user.User;
 import com.project.dogwalker.walkerSearch.dto.WalkerInfoSearchCond;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,12 +18,16 @@ class CustomWalkerSearchRepositoryImplTest {
   private WalkerSearchRepository walkerSearchRepository;
 
   @Test
-  void test(){
+  @DisplayName("elastic insert 성공")
+  void elastic_insert_test(){
+    //이전 데이터 영향받지 않기 위해 삭제후 테스트 진행
+    walkerSearchRepository.deleteAll();
+
     //given
     User user1= User.builder()
-        .userLat(12.0)
+        .userLat(37.300422)
         .userId(1L)
-        .userLnt(3.0)
+        .userLnt(127.074458)
         .userEmail("a1@naver.com")
         .userPhoneNumber("010-1234-1234")
         .userName("test1")
@@ -29,32 +35,27 @@ class CustomWalkerSearchRepositoryImplTest {
         .build();
 
     User user2= User.builder()
-        .userLat(12.5)
+        .userLat(37.3004)
         .userId(2L)
-        .userLnt(3.0)
+        .userLnt(127.074459)
         .userEmail("a1@naver.com")
         .userPhoneNumber("010-1234-1234")
         .userName("test123")
         .userRole(Role.WALKER)
         .build();
-    //walkerSearchRepository.save(WalkerDocument.of(user1));
-   // walkerSearchRepository.save(WalkerDocument.of(user2));
+    walkerSearchRepository.save(WalkerDocument.of(user1));
+    walkerSearchRepository.save(WalkerDocument.of(user2));
 
     WalkerInfoSearchCond searchCond=WalkerInfoSearchCond.builder()
         .walkerName("test")
-        .lat(13.0)
-        .lnt(3.0)
-        .startPage(3)
+        .lat(37.3017387)
+        .lnt(127.0735513)
+        .startPage(0)
         .build();
     //when
-    System.out.println("search start");
     Page <WalkerDocument> walkerDocuments = walkerSearchRepository.searchByName(searchCond);
-    System.out.println("search end");
+
     //then
-    System.out.println("ddddddddddddd");
-    System.out.println(walkerDocuments.getTotalElements());
-    for (WalkerDocument walkerDocument : walkerDocuments) {
-      System.out.println("ddd= "+walkerDocument);
-    }
+    Assertions.assertThat(walkerDocuments.getTotalElements()).isEqualTo(2);
   }
 }
