d3c062e9bd8585dcf8c547a2221ebb49be5be5ed
haebeen <haebing0309@gmail.com>
2023-12-11 21:35:39 +0900

    [refactor] 순환참조 발생

--- src/main/java/com/project/dogwalker/config/WebConfig.java
+++ src/main/java/com/project/dogwalker/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.project.dogwalker.config;
 
+import com.project.dogwalker.member.controller.AuthArgumentResolver;
+import com.project.dogwalker.member.controller.AuthInterceptor;
+import com.project.dogwalker.member.token.JwtTokenProvider;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,15 +14,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-  private final List<HandlerInterceptor> interceptors;
-  private final List<HandlerMethodArgumentResolver> resolvers;
+
+  private final JwtTokenProvider jwtTokenProvider;
 
   @Override
   public void addInterceptors(final InterceptorRegistry registry) {
-    interceptors.forEach(registry::addInterceptor);
+    registry.addInterceptor(new AuthInterceptor(jwtTokenProvider));
   }
   @Override
   public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
-    resolvers.addAll(this.resolvers);
+    resolvers.add(new AuthArgumentResolver(jwtTokenProvider));
   }
 }
