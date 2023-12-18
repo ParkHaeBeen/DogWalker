a4da2f8eba011bd58a0d26310c6acdb2ddddfc98
haebeen <haebing0309@gmail.com>
2023-12-18 20:31:19 +0900

    [refactor] 서비스 수행자 예약 요청/거부 수정

---
+++ src/main/java/com/project/dogwalker/reserve/dto/ReserveStatusRequest.java
@@ -0,0 +1,27 @@
+package com.project.dogwalker.reserve.dto;
+
+import com.project.dogwalker.domain.reserve.WalkerServiceStatus;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Builder
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class ReserveStatusRequest {
+
+  @NotNull
+  private Long reserveId;
+
+  @NotNull
+  private WalkerServiceStatus status;
+}
