7eb94f3b877b8cb972c0406bf9714afcf81e5835
haebeen <haebing0309@gmail.com>
2024-01-07 15:03:44 +0900

    [refactor] Adjust 테이블 수정

--- src/main/resources/db/migration/V4__create_adjust_table.sql
+++ src/main/resources/db/migration/V4__create_adjust_table.sql
@@ -5,7 +5,9 @@ CREATE TABLE `walker_adjust` (
                                  `walker_ttlprice`	bigint  NOT NULL,
                                  `walker_adjust_status`	char(10)  NOT NULL,
                                  `walker_adjust_period_start` timestamp NOT NULL,
-                                 `walker_adjust_period_end` timestamp NOT NULL
+                                 `walker_adjust_period_end` timestamp NOT NULL,
+                                 `created_at`	timestamp NOT	NULL,
+                                 `updated_at`	timestamp	NOT NULL
 );
 
 CREATE TABLE `walker_adjust_detail` (
@@ -13,5 +15,7 @@ CREATE TABLE `walker_adjust_detail` (
                                         `walker_adjust_id`	bigint	NOT NULL,
                                         `walker_reserve_service_id`	bigint	NOT NULL,
                                         `walker_adjust_price`	bigint NOT 	NULL,
-                                        `walker_adjust_status`	char(10) NOT NULL
+                                        `walker_adjust_status`	char(10) NOT NULL,
+                                        `created_at`	timestamp NOT	NULL,
+                                        `updated_at`	timestamp	NOT NULL
 );
