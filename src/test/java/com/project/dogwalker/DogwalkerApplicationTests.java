ea157c3cc1a47b4382eeda444ae9cb1da552105f
haebeen <haebing0309@gmail.com>
2024-01-26 00:21:00 +0900

    [refactor #29] h2 디펜던시 추가

--- src/test/java/com/project/dogwalker/DogwalkerApplicationTests.java
+++ src/test/java/com/project/dogwalker/DogwalkerApplicationTests.java
@@ -1,9 +1,8 @@
 package com.project.dogwalker;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DogwalkerApplicationTests {
 
 	@Test
