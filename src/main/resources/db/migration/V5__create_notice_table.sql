e13b4efb02a565d1180a77d82c3e542dfb3bdfae
haebeen <haebing0309@gmail.com>
2024-01-26 14:41:47 +0900

    [refactor #31] Oauth 정리및 테이블 같이 정리

--- src/main/resources/db/migration/V5__create_notice_table.sql
+++ src/main/resources/db/migration/V5__create_notice_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE `notice` (
                           `notice_id`	BIGINT AUTO_INCREMENT PRIMARY KEY,
                           `user_Id`	bigint	NOT NULL,
                           `notice_type`	char(10) NOT NULL,
-                          `notice_created_at`	timestamp NOT NULL,
+                          `created_at`	timestamp NOT NULL,
                           `notice_path`	varchar(20)	NULL,
                           `notice_check_date`	timestamp	NULL,
-                          `notice_updated_at`	timestamp NOT NULL
+                          `updated_at`	timestamp NOT NULL
 );
