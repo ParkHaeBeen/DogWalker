2a530aac5fe0d0c5ce1369654ed9381c2ce5db2d
haebeen <haebing0309@gmail.com>
2024-01-27 20:01:59 +0900

    [refactor #35] 프로파일 붙이기

--- src/test/java/com/project/dogwalker/domain/user/walker/WalkerInfoResponseRepositoryImplTest.java
+++ src/test/java/com/project/dogwalker/domain/user/walker/WalkerInfoResponseRepositoryImplTest.java
@@ -20,10 +20,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class WalkerInfoResponseRepositoryImplTest {
-
 
   @Autowired
   private UserRepository userRepository;
