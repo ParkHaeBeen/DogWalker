b42cd09788af74d7edd8540c161869e37e5a1a7a
haebeen <haebing0309@gmail.com>
2024-01-29 12:20:06 +0900

    [refactor #37] table 제약조건 수정

--- src/main/resources/db/migration/V4__create_adjust_table.sql
+++ src/main/resources/db/migration/V4__create_adjust_table.sql
@@ -5,7 +5,9 @@ CREATE TABLE `walker_adjust` (
                                  `walker_ttlprice`	bigint  NOT NULL,
                                  `walker_adjust_status`	char(10)  NOT NULL,
                                  `walker_adjust_period_start` timestamp NOT NULL,
-                                 `walker_adjust_period_end` timestamp NOT NULL
+                                 `walker_adjust_period_end` timestamp NOT NULL,
+                                 FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+
 );
 
 CREATE TABLE `walker_adjust_detail` (
@@ -13,28 +15,9 @@ CREATE TABLE `walker_adjust_detail` (
                                         `walker_adjust_id`	bigint	NOT NULL,
                                         `walker_reserve_service_id`	bigint	NOT NULL,
                                         `walker_adjust_price`	bigint NOT 	NULL,
-                                        `walker_adjust_status`	char(10) NOT NULL
+                                        `walker_adjust_status`	char(10) NOT NULL,
+                                        FOREIGN KEY (`walker_adjust_id`) REFERENCES `walker_adjust` (`walker_adjust_id`),
+                                        FOREIGN KEY (`walker_reserve_service_id`) REFERENCES `walker_reserve_service` (`walker_reserve_service_id`)
+
 );
-
-ALTER TABLE `walker_adjust` ADD CONSTRAINT `FK_Users_TO_walker_adjust_1` FOREIGN KEY (
-                                                                                      `user_id`
-    )
-    REFERENCES `users` (
-                        `user_id`
-        );
-
-ALTER TABLE `walker_adjust_detail` ADD CONSTRAINT `FK_walker_adjust_TO_walker_adjust_detail_1` FOREIGN KEY (
-                                                                                                            `walker_adjust_id`
-    )
-    REFERENCES `walker_adjust` (
-                                `walker_adjust_id`
-        );
-
-ALTER TABLE `walker_adjust_detail` ADD CONSTRAINT `FK_walker_reserve_service_TO_walker_adjust_detail_1` FOREIGN KEY (
-                                                                                                                     `walker_reserve_service_id`
-    )
-    REFERENCES `walker_reserve_service` (
-                                         `walker_reserve_service_id`
-        );
-
 
--- src/main/resources/db/migration/V5__create_notice_table.sql
+++ src/main/resources/db/migration/V5__create_notice_table.sql
@@ -1,16 +1,10 @@
 CREATE TABLE `notice` (
                           `notice_id`	BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          `user_Id`	bigint	NOT NULL,
+                          `user_id`	bigint	NOT NULL,
                           `notice_type`	char(10) NOT NULL,
                           `created_at`	timestamp NOT NULL,
                           `notice_path`	varchar(20)	NULL,
                           `notice_check_date`	timestamp	NULL,
-                          `updated_at`	timestamp NOT NULL
+                          `updated_at`	timestamp NOT NULL,
+                          FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 );
-
-ALTER TABLE `notice` ADD CONSTRAINT `FK_Users_TO_notice_1` FOREIGN KEY (
-                                                                        `user_id`
-    )
-    REFERENCES `users` (
-                        `user_id`
-        );
