40da069bde0dd1847b54d6eb01888486b348803a
haebeen <haebing0309@gmail.com>
2024-01-29 12:09:41 +0900

    [refactor #37] Member 관련 user,walker 테이블 제약조건 변경

--- src/main/resources/db/migration/V4__create_adjust_table.sql
+++ src/main/resources/db/migration/V4__create_adjust_table.sql
@@ -15,3 +15,26 @@ CREATE TABLE `walker_adjust_detail` (
                                         `walker_adjust_price`	bigint NOT 	NULL,
                                         `walker_adjust_status`	char(10) NOT NULL
 );
+
+ALTER TABLE `walker_adjust` ADD CONSTRAINT `FK_Users_TO_walker_adjust_1` FOREIGN KEY (
+                                                                                      `user_id`
+    )
+    REFERENCES `users` (
+                        `user_id`
+        );
+
+ALTER TABLE `walker_adjust_detail` ADD CONSTRAINT `FK_walker_adjust_TO_walker_adjust_detail_1` FOREIGN KEY (
+                                                                                                            `walker_adjust_id`
+    )
+    REFERENCES `walker_adjust` (
+                                `walker_adjust_id`
+        );
+
+ALTER TABLE `walker_adjust_detail` ADD CONSTRAINT `FK_walker_reserve_service_TO_walker_adjust_detail_1` FOREIGN KEY (
+                                                                                                                     `walker_reserve_service_id`
+    )
+    REFERENCES `walker_reserve_service` (
+                                         `walker_reserve_service_id`
+        );
+
+
