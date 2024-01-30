0bbbeacedd414d8eb12949d1f75510e46fc78fcd
haebeen <haebing0309@gmail.com>
2024-01-30 10:41:31 +0900

    [refactor #41] globalexceptionhandler 정리

--- src/main/java/com/project/dogwalker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/dogwalker/exception/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import com.project.dogwalker.exception.member.MemberException;
 import com.project.dogwalker.exception.notice.NoticeNotFoundException;
 import com.project.dogwalker.exception.notice.SseException;
 import com.project.dogwalker.exception.reserve.LockException;
+import com.project.dogwalker.exception.reserve.ReserveException;
 import com.project.dogwalker.exception.unauth.TokenException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -28,31 +29,31 @@ public class GlobalExceptionHandler {
   private static final String LOG_ERROR_MESSAGE = "Class : {}, Code : {}, Message : {}";
 
   @ExceptionHandler(ImgUploadFailException.class)
-  public ResponseEntity<ExceptionResponse> handleImgException(final ImgUploadFailException e) {
-    log.info(LOG_ERROR_MESSAGE , e.getClass() , e.getErrorCode() , e.getErrorMessage());
+  public ResponseEntity<ExceptionResponse> handleException(final ImgUploadFailException e) {
+    log.info(LOG_ERROR_MESSAGE , e.getClass() , e.getErrorCode() , e.getErrorCode().getValue());
     return ResponseEntity.badRequest().body(ExceptionResponse.from(e));
   }
 
   @ExceptionHandler(FeignBadRequestException.class)
-  public ResponseEntity<ExceptionResponse> handleFeignBadRequest(final FeignBadRequestException e){
-    log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorMessage());
+  public ResponseEntity<ExceptionResponse> handleException(final FeignBadRequestException e){
+    log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorCode().getValue());
     return ResponseEntity.badRequest().body(ExceptionResponse.from(e));
   }
 
   @ExceptionHandler(FeignCommonException.class)
-  public ResponseEntity<ExceptionResponse> handleFeignCommon(final FeignCommonException e){
+  public ResponseEntity<ExceptionResponse> handleException(final FeignCommonException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorMessage());
     return ResponseEntity.badRequest().body(ExceptionResponse.from(e));
   }
 
   @ExceptionHandler(FeignErrorParseException.class)
-  public ResponseEntity<ExceptionResponse> handleFeignParse(final FeignErrorParseException e){
+  public ResponseEntity<ExceptionResponse> handleException(final FeignErrorParseException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorMessage());
     return ResponseEntity.badRequest().body(ExceptionResponse.from(e));
   }
 
   @ExceptionHandler(FeignNotFoundException.class)
-  public ResponseEntity<ExceptionResponse> handleFeignNotFound(final FeignNotFoundException e){
+  public ResponseEntity<ExceptionResponse> handleException(final FeignNotFoundException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorMessage());
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ExceptionResponse.from(e));
   }
@@ -64,49 +65,54 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(AuthMemberException.class)
-  public ResponseEntity<TokenResponse> hanldeAuthException(final AuthMemberException e){
+  public ResponseEntity<TokenResponse> handleException(final AuthMemberException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getMessage());
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(TokenResponse.from(e,e.getToken()));
   }
 
   @ExceptionHandler(MemberException.class)
-  public ResponseEntity<ExceptionResponse> handleMemberException(final MemberException e){
+  public ResponseEntity<ExceptionResponse> handleException(final MemberException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getMessage());
     return ResponseEntity.badRequest().body(ExceptionResponse.from(e));
   }
+  @ExceptionHandler(ReserveException.class)
+  public ResponseEntity<ExceptionResponse> handleException(final ReserveException e){
+    log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ExceptionResponse.from(e));
+  }
 
   @ExceptionHandler(LockException.class)
-  public ResponseEntity<ExceptionResponse> handleLock(final LockException e){
+  public ResponseEntity<ExceptionResponse> handleException(final LockException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorMessage());
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ExceptionResponse.from(e));
   }
 
   @ExceptionHandler(ReserveBatchException.class)
-  public ResponseEntity<ExceptionResponse> handleReserveBatch(final ReserveBatchException e){
+  public ResponseEntity<ExceptionResponse> handleException(final ReserveBatchException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorMessage());
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ExceptionResponse.from(e));
   }
 
   @ExceptionHandler(TokenException.class)
-  public ResponseEntity<ExceptionResponse> handleTRefreshTokenExpired(final TokenException e){
+  public ResponseEntity<ExceptionResponse> handleException(final TokenException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getMessage());
     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ExceptionResponse.from(e));
   }
 
   @ExceptionHandler(NoticeNotFoundException.class)
-  public ResponseEntity<ExceptionResponse> handleNoticeNotFound(final NoticeNotFoundException e){
+  public ResponseEntity<ExceptionResponse> handleException(final NoticeNotFoundException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorMessage());
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ExceptionResponse.from(e));
   }
 
   @ExceptionHandler(SseException.class)
-  public ResponseEntity<ExceptionResponse> handleSse(final SseException e){
+  public ResponseEntity<ExceptionResponse> handleException(final SseException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getErrorCode(),e.getErrorMessage());
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ExceptionResponse.from(e));
   }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<?> handleArgumentNotValue(final MethodArgumentNotValidException e){
+  public ResponseEntity<?> handleException(final MethodArgumentNotValidException e){
     log.info(LOG_ERROR_MESSAGE,e.getClass(),e.getStatusCode(),e.getMessage());
     return ResponseEntity.status(e.getStatusCode()).body(e);
   }
