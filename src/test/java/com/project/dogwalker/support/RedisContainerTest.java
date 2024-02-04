2bab26e8a06194f26a44ced9166154a58689a563
haebeen <haebing0309@gmail.com>
2024-02-04 22:28:23 +0900

    [Bug #52] testcontainer 수정

--- src/test/java/com/project/dogwalker/support/RedisContainerTest.java
+++ src/test/java/com/project/dogwalker/support/RedisContainerTest.java
@@ -1,28 +1,33 @@
 package com.project.dogwalker.support;
 
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
+@DisplayName("Redis Test Containers")
+@ActiveProfiles(profiles = "test")
+@Configuration
 public class RedisContainerTest {
   static final String REDIS_IMAGE = "redis:7.0.8-alpine";
   @Container
   static final GenericContainer <?> REDIS_CONTAINER;
 
   static {
-
     REDIS_CONTAINER = new GenericContainer<>(REDIS_IMAGE)
-        .withExposedPorts(6379) // 여기서 6380은 호스트에 노출될 포트, 6379는 컨테이너 내부의 포트
+        .withExposedPorts(6379)
         .withReuse(true);
-    REDIS_CONTAINER.start(); // (4)
+    REDIS_CONTAINER.start();
+    System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
+    System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
   }
 
-  @DynamicPropertySource // (5)
+/*  @DynamicPropertySource // (5)
   public static void overrideProps(DynamicPropertyRegistry registry){
     registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
     registry.add("spring.data.redis.port", () -> ""+REDIS_CONTAINER.getMappedPort(6379));
-  }
+  }*/
 }
